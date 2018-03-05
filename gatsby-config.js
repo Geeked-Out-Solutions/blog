928bac2bcb2ad9aba46421a9125131def7465923
Brian Hopkins <brianhh1230@gmail.com>
2018-03-05 09:44:21 -0500

    adding in google tracking for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-102614039-1",
+      // Puts tracking script in the head instead of the body
+      head: false,
+    },
+  },
   ],
 }
