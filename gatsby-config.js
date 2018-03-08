5916fb597cc4aaa23b797d0028419d784c22d93c
Brian Hopkins <brianhh1230@gmail.com>
2018-03-08 09:12:56 -0500

    fixing dev branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,12 +102,12 @@ module.exports = {
         display: "minimal-ui",
         icons: [
           {
-            src: "/logos/logo.png",
+            src: "/logos/logo-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/logos/logo.png",
+            src: "/logos/logo-512.png",
             sizes: "512x512",
             type: "image/png"
           }
--- src/pages/blog/2018-03-03-chatbot-tutorials-coming.md
+++
@@ -1,9 +0,0 @@
----
-templateKey: blog-post
-title: Chatbot Tutorials Coming...
-date: 2018-03-04T02:04:13.088Z
-description: New site revamp underway
----
-![chatbot](/img/chatbot.jpg)
-
-Now that the site is being revamped and back up new posts will be up shortly.
