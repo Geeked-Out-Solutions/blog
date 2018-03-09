fb0a22a178c4e986e459fd0e67908f6a17d6e5e3
Brian Hopkins <brianhh1230@gmail.com>
2018-03-08 21:18:35 -0500

    testing new disqus package

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,32 +1,13 @@
 import React, { Component } from "react";
-import ReactDisqusComments from "react-disqus-comments";
+import Disqus from "disqus-react";
 import Card from "react-md/lib/Cards/Card";
 import CardTitle from "react-md/lib/Cards/CardTitle";
 import CardText from "react-md/lib/Cards/CardText";
 import Avatar from "react-md/lib/Avatars";
 import FontIcon from "react-md/lib/FontIcons";
-import Snackbar from "react-md/lib/Snackbars";
 import config from "../../../data/SiteConfig";
 
-class Disqus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toasts: []
-    };
-    this.notifyAboutComment = this.notifyAboutComment.bind(this);
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
-  }
-
-  onSnackbarDismiss() {
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
-  }
-  notifyAboutComment() {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text: "New comment available!" });
-    this.setState({ toasts });
-  }
+class Article extends Component {
   render() {
     const { postNode, expanded } = this.props;
     if (!config.disqusShortname) {
@@ -44,22 +25,13 @@ class Disqus extends Component {
           expander={!expanded}
         />
         <CardText expandable={!expanded}>
-          <ReactDisqusComments
-            shortname={config.disqusShortname}
-            identifier={post.title}
-            title={post.title}
-            url={url}
-            category_id={post.category_id}
-            onNewComment={this.notifyAboutComment}
-          />
+          <Disqus.CommentCount shortname={config.disqusShortname} config={config}>
+                    Comments
+          </Disqus.CommentCount>
         </CardText>
-        <Snackbar
-          toasts={this.state.toasts}
-          onDismiss={this.onSnackbarDismiss}
-        />
       </Card>
     );
   }
 }
 
-export default Disqus;
+export default Article;
