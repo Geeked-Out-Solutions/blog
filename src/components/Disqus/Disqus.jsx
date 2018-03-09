d5bc2e22c2468a0a2cd6da483da00095bfde4b10
Brian Hopkins <brianhh1230@gmail.com>
2018-03-08 21:22:38 -0500

    reverting change

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
-import Disqus from "disqus-react";
+import ReactDisqusComments from "react-disqus-comments";
 import Card from "react-md/lib/Cards/Card";
 import CardTitle from "react-md/lib/Cards/CardTitle";
 import CardText from "react-md/lib/Cards/CardText";
 import Avatar from "react-md/lib/Avatars";
 import FontIcon from "react-md/lib/FontIcons";
+import Snackbar from "react-md/lib/Snackbars";
 import config from "../../../data/SiteConfig";
 
-class Article extends Component {
+class Disqus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toasts: []
+    };
+    this.notifyAboutComment = this.notifyAboutComment.bind(this);
+    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
+  }
+
+  onSnackbarDismiss() {
+    const [, ...toasts] = this.state.toasts;
+    this.setState({ toasts });
+  }
+  notifyAboutComment() {
+    const toasts = this.state.toasts.slice();
+    toasts.push({ text: "New comment available!" });
+    this.setState({ toasts });
+  }
   render() {
     const { postNode, expanded } = this.props;
     if (!config.disqusShortname) {
@@ -25,13 +44,22 @@ class Article extends Component {
           expander={!expanded}
         />
         <CardText expandable={!expanded}>
-          <Disqus.CommentCount shortname={config.disqusShortname} config={config}>
-                    Comments
-          </Disqus.CommentCount>
+          <ReactDisqusComments
+            shortname={config.disqusShortname}
+            identifier={post.title}
+            title={post.title}
+            url={url}
+            category_id={post.category_id}
+            onNewComment={this.notifyAboutComment}
+          />
         </CardText>
+        <Snackbar
+          toasts={this.state.toasts}
+          onDismiss={this.onSnackbarDismiss}
+        />
       </Card>
     );
   }
 }
 
-export default Article;
+export default Disqus;
